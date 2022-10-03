ed99e93bf4e737c9dbde597adaabf36bcb40b0cb
Jake Casartelli <casartellijake@yahoo.com>
2022-10-03 17:53:26 -0400

    added more detail in js comments

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //make sure html is processed with window event listener
 window.addEventListener('DOMContentLoaded', () => {
+    //use Array.from to create a proper array
     const tiles = Array.from(document.querySelectorAll('.tile'));
     const playerDisplay = document.querySelector('.display-player');
     const resetButton = document.querySelector('#reset');
@@ -8,7 +9,7 @@ window.addEventListener('DOMContentLoaded', () => {
     let board = ['', '', '', '', '', '', '', '', ''];
     let currentPlayer = 'X';
     let isGameActive = true;
-
+// variables store end game state
     const PLAYERX_WON = 'PLAYERX_WON';
     const PLAYERO_WON = 'PLAYERO_WON';
     const TIE = 'TIE';
@@ -31,7 +32,13 @@ window.addEventListener('DOMContentLoaded', () => {
         [0, 4, 8],
         [2, 4, 6]
     ];
-//loop through winning conditions
+//loop through winning conditions array
+//From Youtube: https://www.youtube.com/watch?v=B3pmT7Cpi24
+//check if array has connectors for the sub arrays
+//continue skips the iteration
+//if the variables are true then roundWon will equal true and execute the if statetement
+//that takes in roundWon as an argument
+
     function handleResultValidation() {
         let roundWon = false;
         for (let i = 0; i <= 7; i++) {
@@ -54,6 +61,7 @@ window.addEventListener('DOMContentLoaded', () => {
             return;
         }
 //if tie this will execute
+//and has no empty spaces
     if (!board.includes(''))
         announce(TIE);
     }
@@ -91,7 +99,8 @@ window.addEventListener('DOMContentLoaded', () => {
         playerDisplay.classList.add(`player${currentPlayer}`);
     }
 // represents a turn in the game as a function
-
+//will be called when user clicks on a tile
+//updates the board and changes the player using changePlayer function
     const userAction = (tile, index) => {
         if(isValidAction(tile) && isGameActive) {
             tile.innerText = currentPlayer;
@@ -102,6 +111,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
     //resets the game board
+    // recreate board and have player x start every game
     const resetBoard = () => {
         board = ['', '', '', '', '', '', '', '', ''];
         isGameActive = true;
@@ -118,6 +128,7 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     }
 //add forEach function to listen to a click and itll refrence the tile and index of it
+//adds it to each tile
     tiles.forEach( (tile, index) => {
         tile.addEventListener('click', () => userAction(tile, index));
     });
